dd991c27b35391d15a3c31671f4ed94d5e25c11a
J.C. Hiatt <jc@jchiatt.com>
2018-08-22 15:59:53 -0500

    add header and footer files, convert App to class component

---
+++ src/Components/Layouts/Footer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Footer = () => (
+  <div>
+    <h1>Footer</h1>
+  </div>
+)
+
+export default Footer;
---
+++ src/Components/Layouts/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Header = () => (
+  <div>
+    <h1>Header</h1>
+  </div>
+)
+
+export default Header;
